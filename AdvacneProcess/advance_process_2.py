0a653c0ba0ef38062d80b0c4d3fa7aeecb469e93
Jerry-Github-Cloud <oo12374@gmail.com>
2023-04-07 12:01:10 +0800

    1.advance_process_2.py: Solve wafer sampling & fill the sample panel.   Report: 'https://docs.google.com/presentation/d/1ba4uNlvZbuo9QowlMpTTebamvV9cpMHi0hHAn-1Xqqk/edit?usp=sharing'

--- AdvacneProcess/advance_process_2.py
+++ AdvacneProcess/advance_process_2.py
@@ -3,13 +3,13 @@ import json
 import numpy as np
 from ortools.sat.python import cp_model
 
-def solve(all_block, wafer_width, wafer_height):
+def solve(all_block, wafer_width, wafer_height, time_limit=60, num_thread=1):
     ### wafer sampling
     # Number of blocks
     n = len(all_block)
     print(f"n: {n}")
 
-    # wafer Variables
+    # wafer variables
     all_wafer_x_st, all_wafer_y_st, all_wafer_x_ed, all_wafer_y_ed, sampled, all_ng = [], [], [], [], [], []
     for i, block in enumerate(all_block):
         if block['x'] == None:
@@ -40,7 +40,7 @@ def solve(all_block, wafer_width, wafer_height):
         model.Add(all_wafer_x_ed[i] <= wafer_width).OnlyEnforceIf(sampled[i])
         model.Add(all_wafer_y_ed[i] <= wafer_height).OnlyEnforceIf(sampled[i])
 
-    # wafer Non-overlapping constraints
+    # wafer non-overlapping constraints
     for i in range(n):
         for j in range(i + 1, n):
             wafer_bx_ij = model.NewBoolVar(f"wafer_bx_{i}_{j}")
@@ -55,11 +55,8 @@ def solve(all_block, wafer_width, wafer_height):
 
             model.AddBoolOr([wafer_bx_ij.Not(), wafer_bx_ji.Not(),
                             wafer_by_ij.Not(), wafer_by_ji.Not()])
-
-    ### place to sample panel
-    panel_width = 6
-    panel_height = 6
-    # panel Variables
+    
+    # panel variables
     all_panel_x_st, all_panel_y_st, all_panel_x_ed, all_panel_y_ed, on_panel = [], [], [], [], []
     for i, block in enumerate(all_block):
         all_panel_x_st.append(model.NewIntVar(0, panel_width - block['w'], f"panel_x{i}"))
@@ -71,8 +68,9 @@ def solve(all_block, wafer_width, wafer_height):
     # exclude ng
     for i, block in enumerate(all_block):
         model.AddBoolAnd([sampled[i], all_ng[i].Not()]).OnlyEnforceIf(on_panel[i])
+        model.AddBoolOr([sampled[i].Not(), all_ng[i]]).OnlyEnforceIf(on_panel[i].Not())
+
     # panel width & height constraints
-    
     for i, block in enumerate(all_block):
         model.Add(all_panel_x_ed[i] == all_panel_x_st[i] + block['w']).OnlyEnforceIf(on_panel[i])
         model.Add(all_panel_y_ed[i] == all_panel_y_st[i] + block['h']).OnlyEnforceIf(on_panel[i])
@@ -119,7 +117,7 @@ def solve(all_block, wafer_width, wafer_height):
     model.AddDivisionEquality(
         block_utilization,
         num_blocks_sampled * scale,
-        n)
+        sum(not block['ng'] for i, block in enumerate(all_block)))
 
     # model.Maximize(wafer_coverage * (1 / scale) - 
     #                block_utilization * (1 / scale))
@@ -128,10 +126,14 @@ def solve(all_block, wafer_width, wafer_height):
 
     # Solve the model
     solver = cp_model.CpSolver()
+    solver.parameters.log_search_progress = False
+    solver.parameters.max_time_in_seconds = time_limit
+    solver.parameters.num_workers = num_thread
+    print("Solve")
     status = solver.Solve(model)
 
     # Print
-    if status == cp_model.OPTIMAL:
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         block_positions_on_wafer = [(solver.Value(all_wafer_x_st[i]), solver.Value(all_wafer_y_st[i]))
                      for i in range(n) if solver.Value(on_panel[i])]
         block_positions_on_panel = [(solver.Value(all_panel_x_st[i]), solver.Value(all_panel_y_st[i]))
@@ -153,8 +155,8 @@ def solve(all_block, wafer_width, wafer_height):
             block['y'] = solver.Value(all_wafer_y_st[i])
             all_block_sampled.append(block)
         result = {}
-        result['width'] = wafer_width
-        result['height'] = wafer_height
+        result["width"] = wafer_width
+        result["height"] = wafer_height
         result["block"] = all_block_sampled
         with open(os.path.join(result_path, file_name), 'w') as fp:
             json.dump(result, fp, indent=4)
@@ -166,13 +168,22 @@ if __name__ == "__main__":
     data_path = "block_data"
     result_path = "result"
     file_name = "0003.json"
-    # file_name = "0005.json"
+    # file_name = "0001_d=100.json"
+    # file_name = "0001_d=20.json"
     # file_name = "0001_d=10.json"
+    # file_name = "0001_d=12.json"
+    # file_name = "0002_d=10.json"
+    # file_name = "0001_d=5.json"
+
     # Create the model
     model = cp_model.CpModel()
     with open(os.path.join(data_path, file_name), 'r') as fp:
         data = json.load(fp)
-    wafer_width = data["width"]
-    wafer_height = data["height"]
+    wafer_width = data["wafer_width"]
+    wafer_height = data["wafer_height"]
+    panel_width = data["panel_width"]
+    panel_height = data["panel_height"]
     all_block = data["block"]
-    solve(all_block, wafer_width, wafer_height)
+    time_limit = 3600 * 24
+    # solve(all_block, wafer_width, wafer_height)
+    solve(all_block, wafer_width, wafer_height, time_limit=time_limit, num_thread=8)
